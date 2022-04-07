3c69052e302b33d64d0cfe5b5875028a85522d92
natalia <88321926+Elensarr@users.noreply.github.com>
2022-04-07 12:21:47 +1200

    chore: sec5 review

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -19,10 +19,9 @@ export class AccountService {
 
   constructor(private http: HttpClient) { }
 
-  login(model: any) {
+  public login(model: any) {
     return this.http.post(this.baseUrl + 'account/login', model)
-      .pipe(
-        map((response: User) => {
+      .pipe(map((response: User) => {
           const user = response;
           if (user) {
             localStorage.setItem('user', JSON.stringify(user));
@@ -49,7 +48,7 @@ export class AccountService {
             localStorage.setItem('user', JSON.stringify(user));
             this.currentUserSource.next(user)
           }
-          return user;
+         return user;
         })
       )
   }
