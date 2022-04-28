38710477376d1c26787b44b90e6235d243f03565
natalia <88321926+Elensarr@users.noreply.github.com>
2022-04-28 12:11:44 +1200

    chore: api done

--- API/Extentions/IdentityServiceExtentions.cs
+++ API/Extentions/IdentityServiceExtentions.cs
@@ -37,6 +37,19 @@ namespace API.Extentions
                     };
                 }
                 );
+
+            services.AddAuthorization(
+                opt =>
+                {
+                    opt.AddPolicy("RequireAdminRole",
+                        policy =>
+                        policy.RequireRole("Admin"));
+                    opt.AddPolicy("ModeratePhotoRole", policy =>
+                        policy.RequireRole("Admin", "Moderator"));
+                }
+                );
+
+
             return services;
         }
     }
